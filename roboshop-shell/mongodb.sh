f705ee62fd6ad3587eb45fe9b12e9cece60546a8
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-05-31 00:03:44 +0530

    script

--- roboshop-shell/mongodb.sh
+++ roboshop-shell/mongodb.sh
@@ -1,9 +1,9 @@
-echo -e "\e[33mCopy mongodb Repo file \e[0m"
+echo -e "\e[33mCopy Mongodb Repo file \e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
-echo -e "\e[33mInstalling mongodb service\e[0m"
+echo -e "\e[33mInstalling MongoDB Service\e[0m"
 yum install mongodb-org -y 
 ## Modify the config file
-echo -e "\e[33mStart monodb service\e[0m"
+echo -e "\e[33mStart MongoDB Service\e[0m"
 systemctl enable mongod 
 systemctl restart mongod
 
