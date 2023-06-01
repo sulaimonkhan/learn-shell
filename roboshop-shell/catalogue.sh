177fe294e383603a214f9cfe03c992aed44b6268
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-01 16:09:27 +0530

    scrpit

--- roboshop-shell/catalogue.sh
+++ roboshop-shell/catalogue.sh
@@ -1,5 +1,5 @@
 echo -e "\e[33mConfiguring Nodejs Repos\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
 echo -e "\e[33m Install Nodejs\e[0m"
 yum install nodejs -y  &>>/tmp/roboshop.log
@@ -7,13 +7,13 @@ yum install nodejs -y  &>>/tmp/roboshop.log
 echo -e "\e[33m Add Application User\e[0m"
 useradd roboshop  &>>/tmp/roboshop.log
 
-echo -e "\e[33m Create Application Dirctory \e[0m"
+echo -e "\e[33m Create Application Dirctory\e[0m"
 rm -rf /app  &>>/tmp/roboshop.log
 mkdir /app
 
 echo -e "\e[33m Download Application Content\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
-cdd /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>/tmp/roboshop.log
+cd /app
 
 
 echo -e "\e[33m Extract Application Content\e[0m"
@@ -26,16 +26,16 @@ npm install  &>>/tmp/roboshop.log
 echo -e "\e[33m Setup systemD Service\e[0m"
 cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
 
-echo -e "\e[33m Start Catalogue Service \e[0m"
+echo -e "\e[33m Start Catalogue Service\e[0m"
 systemctl daemon-reload  &>>/tmp/roboshop.log
 systemctl enable catalogue  &>>/tmp/roboshop.log
 systemctl restart catalogue  &>>/tmp/roboshop.log
 
-echo -e "\e[33m Copy MongoDB Repo file \e[0m"
+echo -e "\e[33m Copy MongoDB Repo file\e[0m"
 cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/roboshop.log
 
-echo -e "\e[33m Install MongoDB Client \e[0m"
+echo -e "\e[33m Install MongoDB Client\e[0m"
 yum install mongodb-org-shell -y  &>>/tmp/roboshop.log
 
-echo -e "\e[33m Load Schema \e[0m"
+echo -e "\e[33m Load Schema\e[0m"
 mongo --host mongodb-dev.devopsb72.site </apps/schema/catalogue.js  &>>/tmp/roboshop.log
