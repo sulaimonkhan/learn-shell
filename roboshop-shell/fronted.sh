5b9922ff8a4565e1f8b7bb904009f7f20508a6d9
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-01 15:21:06 +0530

    scrpit

--- roboshop-shell/fronted.sh
+++ roboshop-shell/fronted.sh
@@ -5,15 +5,14 @@ echo -e "\e[33mRemoving old App content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log 
 
 echo -e "\e[33mDownloading Fronted content\e[0m"]
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log 
-
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log 
 
 echo -e "\e[33mExtract Fronted content\e[0m"
-cd /usr/share/nginx/html &>>/tmp/roboshop.log  
+cd /usr/share/nginx/html   
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
 echo -e "\e[33mUpdate Fronted Configuration\e[0m"
-cp /home/centos/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf 
 
 echo -e "\e[33mStarting Nginx server\e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log  
