508a2bd4ab70842175227b006a7bec6924d6f592
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-01 22:43:56 +0530

    scrpit

--- roboshop-shell/shipping.sh
+++ roboshop-shell/shipping.sh
@@ -13,6 +13,7 @@ curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shippin
 cd /app
 
 echo -e "\e[33m Extract Application Content \e[0m"
+cd /app
 unzip /tmp/shipping.zip &>>/tmp/roboshop.log
 
 echo -e "\e[33m Download Maven Dependencies \e[0m"
