4c0609142328abe6c72ddba0b787a88d6547e4fa
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-01 19:35:10 +0530

    xc

--- roboshop-shell/user.sh
+++ roboshop-shell/user.sh
@@ -1,42 +1,31 @@
-echo -e "\e[33mConfiguring NodeJS Repos\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
-echo -e "\e[33m Install NodeJS\e[0m"
-yum install nodejs -y  &>>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e "\e[33m Add Application User\e[0m"
-useradd roboshop  &>>/tmp/roboshop.log
+yum install nodejs -y
 
-echo -e "\e[33m Create Application Directory\e[0m"
-rm -rf /app  &>>/tmp/roboshop.log 
-mkdir /app
+useradd roboshop
 
-echo -e "\e[33m Download Application Content\e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
-cd /app
+mkdir /app 
 
-echo -e "\e[33m Extract Application Content\e[0m"
-unzip /tmp/user.zip &>>/tmp/roboshop.log
-cd /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip 
+cd /app 
+unzip /tmp/user.zip
 
-echo -e "\e[33m Install NodeJS Dependencies\e[0m"
-npm install &>>/tmp/roboshop.log
 
-echo -e "\e[33m Setup systemD service \e[0m"
-cp user.s/root/learn-shell/roboshop-shell/user.service /etc/systemd/system/user.service &>>/tmp/roboshop.log 
+cd /app 
+npm install 
 
-echo -e "\e[33m Start User Service\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable user   &>>/tmp/roboshop.log
-systemctl restart user  &>>/tmp/roboshop.log 
+cp /root/learn-shell/roboshop-shell/user.service /etc/systemd/system/user.service
 
-echo -e "\e[33m Copy MongoDB Repo file \e[0m"
-cp /root/learn-shell/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+systemctl daemon-reload
 
-echo -e "\e[33m Install MongoDB Client \e[0m"
-yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+systemctl enable user 
+systemctl restart user
 
-echo -e "\e[33m Load Schema \e[0m"
-mongo --host mongodb-dev.devopsb72.site	</app/schema/user.js &>>/tmp/roboshop.log
+cp /root/learn-shell/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+yum install mongodb-org-shell -y
+
+mongo --host mongodb-dev.devopsb72.site </app/schema/user.js
 
 
