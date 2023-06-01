1f985fd92b6a90379f7f98c252ad050e52bf6de9
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-01 14:01:06 +0530

    scrpit

--- roboshop-shell/fronted.sh
+++ roboshop-shell/fronted.sh
@@ -8,11 +8,12 @@ echo -e "\e[33mDownloading Fronted content\e[0m"]
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log 
 
 
-echo -e "\e[33mExtract Fronted content\e[0m"]
+echo -e "\e[33mExtract Fronted content\e[0m"
 cd /usr/share/nginx/html &>>/tmp/roboshop.log  
-unzip /tmp/frontend.zip &>>/tmp/roboshop.log 
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-## we need to copy config files s               
+echo -e "\e[33mUpdate Fronted Configuration\e[0m"
+cp /home/centos/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33mStarting Nginx server\e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log  
--- roboshop-shell/mongodb.sh
+++ roboshop-shell/mongodb.sh
@@ -1,5 +1,5 @@
 echo -e "\e[33mCopy MongoDB Repo file\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log 
+cp /home/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log 
 
 
 echo -e "\e[33mInstalling MongoDB Server\e[0m"
