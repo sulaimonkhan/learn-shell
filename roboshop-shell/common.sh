90508f9f75c992b097026fdc2aa896e64e5d68ce
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-05 16:01:22 +0530

    scrpit

--- roboshop-shell/common.sh
+++ roboshop-shell/common.sh
@@ -7,13 +7,11 @@ nodejs () {
   echo -e "${color} Configuring Nodejs Repos ${nocolor}"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>$log_file 
 
-
   echo -e "${color} Install Nodejs ${nocolor}"
   yum install nodejs -y  &>>$log_file
 
   echo -e "${color} Add Application User ${nocolor}"
   useradd roboshop  &>>$log_file
-
 
   echo -e "${color} Create Application Dirctory ${nocolor}"
   rm -rf ${app_path}  &>>$log_file
@@ -22,7 +20,6 @@ nodejs () {
   echo -e "${color} Download Application Content ${nocolor}"
   curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>$log_file
   cd ${app_path}
-
 
   echo -e "${color} Extract Application Content ${nocolor}"
   unzip /tmp/$component.zip &>>$log_file
