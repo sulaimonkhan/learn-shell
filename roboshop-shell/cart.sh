ff53d3dd91e2df959478e4c509974c36f086894a
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-05 13:03:36 +0530

    scrpit

--- roboshop-shell/cart.sh
+++ roboshop-shell/cart.sh
@@ -1,32 +1,35 @@
-echo -e "\e[33m Configuring NodeJS Repos\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/roboshop.log
+source common.sh
+component=cart
 
-echo -e "\e[33m Install NodeJS\e[0m"
-yum install nodejs -y  &>>/tmp/roboshop.log
+echo -e "${color} Configuring NodeJS Repos ${nocolor}"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${log_file}
 
-echo -e "\e[33m Add Application Cart\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+echo -e  "${color} Install NodeJS ${nocolor}"
+yum install nodejs -y   &>>${log_file}
 
-echo -e "\e[33m Create Application Directory\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app 
+echo -e  "${color} Add Application ${componenet} ${nocolor}"
+useradd roboshop  &>>${log_file}
 
-echo -e "\e[33m Download Application Content \e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
-cd /app 
+echo -e  "${color} Create Application Directory ${nocolor}"
+rm -rf ${app_path}  &>>${log_file}
+mkdir ${app_path} 
 
-echo -e "\e[33m Extract Application Content\e[0m"
-unzip /tmp/cart.zip &>>/tmp/roboshop.log
-cd /app
+echo -e "${color} Download Application Content ${nocolor}"
+curl -L -o /tmp/${componenet}.zip https://roboshop-artifacts.s3.amazonaws.com/${componenet}.zip  &>>${log_file}
+cd ${app_path} 
 
-echo -e "\e[33m Install NodeJS Dependencies\e[0m"
-npm install &>>/tmp/roboshop.log
+echo -e "${color} Extract Application Content ${nocolor}"
+unzip /tmp/${componenet}.zip  &>>${log_file}
+cd ${app_path}
 
-echo -e "\e[33m Setup Systemd Service \e[0m"
-cp /root/learn-shell/roboshop-shell/cart.service /etc/systemd/system/cart.service  &>>/tmp/roboshop.log
+echo -e "${color} Install NodeJS Dependencies ${nocolor}"
+npm install  &>>${log_file}
+
+echo -e "${color} Setup Systemd Service ${nocolor}"
+cp /root/learn-shell/roboshop-shell/${componenet}.service /etc/systemd/system/${componenet}.service   &>>${log_file}
 
 
-echo -e "\e[33m Start Cart Service \e[0m"
-systemctl daemon-reload  &>>/tmp/roboshop.log
-systemctl enable cart    &>>/tmp/roboshop.log
-systemctl restart cart   &>>/tmp/roboshop.log
+echo -e "${color} Start ${componenet} Service ${nocolor}"
+systemctl daemon-reload   &>>${log_file}
+systemctl enable ${componenet}     &>>${log_file}
+systemctl restart ${componenet}    &>>${log_file}
