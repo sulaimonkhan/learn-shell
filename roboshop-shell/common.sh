19481c18d81c69ad58bfaf837cf19eb4bb143019
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-05 16:22:20 +0530

    scrpit

--- roboshop-shell/common.sh
+++ roboshop-shell/common.sh
@@ -3,36 +3,36 @@ nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
  
- nodejs () {
+nodejs () {
 
-   echo -e "${color} Configuring Nodejs Repos ${nocolor}"
-   curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>$log_file 
+  echo -e "${color} Configuring Nodejs Repos ${nocolor}"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>$log_file 
 
-   echo -e "${color} Install Nodejs ${nocolor}"
-   yum install nodejs -y  &>>$log_file
+  echo -e "${color} Install Nodejs ${nocolor}"
+  yum install nodejs -y  &>>$log_file
 
-   echo -e "${color} Add Application User ${nocolor}"
-   useradd roboshop  &>>$log_file
+  echo -e "${color} Add Application User ${nocolor}"
+  useradd roboshop  &>>$log_file
 
-   echo -e "${color} Create Application Dirctory ${nocolor}"
-   rm -rf ${app_path}  &>>$log_file
-   mkdir ${app_path}
+  echo -e "${color} Create Application Dirctory ${nocolor}"
+  rm -rf ${app_path}  &>>$log_file
+  mkdir ${app_path}
 
-   echo -e "${color} Download Application Content ${nocolor}"
-   curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>$log_file
-   cd ${app_path}
+  echo -e "${color} Download Application Content ${nocolor}"
+  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>$log_file
+  cd ${app_path}
 
-   echo -e "${color} Extract Application Content ${nocolor}"
-   unzip /tmp/$component.zip &>>$log_file
-   cd ${app_path}
+  echo -e "${color} Extract Application Content ${nocolor}"
+  unzip /tmp/$component.zip &>>$log_file
+  cd ${app_path}
 
-   echo -e "${color} Install Nodejs Dependencies ${nocolor}"
-   npm install  &>>$log_file
+  echo -e "${color} Install Nodejs Dependencies ${nocolor}"
+  npm install  &>>$log_file
 
-   echo -e "${color} Setup systemD Service ${nocolor}"
-   cp /root/learn-shell/roboshop-shell/$component.service /etc/systemd/system/$component.service  &>>$log_file
+  echo -e "${color} Setup systemD Service ${nocolor}"
+  cp /root/learn-shell/roboshop-shell/$component.service /etc/systemd/system/$component.service  &>>$log_file
 
-   echo -e "${color} Start $component Service ${nocolor}"
-   systemctl daemon-reload  &>>$log_file
-   systemctl enable $component  &>>$log_file
-   systemctl restart $component  &>>$log_file
+  echo -e "${color} Start $component Service ${nocolor}"
+  systemctl daemon-reload  &>>$log_file
+  systemctl enable $component  &>>$log_file
+  systemctl restart $component  &>>$log_file
