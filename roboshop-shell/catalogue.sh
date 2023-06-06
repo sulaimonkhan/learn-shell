dbcc3eea778737bbc32d48ceeabbe059417fd254
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-06 21:28:22 +0530

    scrpit

--- roboshop-shell/catalogue.sh
+++ roboshop-shell/catalogue.sh
@@ -1,8 +1,37 @@
 source common.sh 
 component=catalogue 
 
+echo -e "${color} Configure NodeJS Repos ${nocolor"}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 
-nodejs
+echo -e "${color} Install NodeJS ${nocolor}"
+yum install nodejs -y &>>$log_file
+
+echo -e "${color} Add Application User ${nocolor}"
+useradd roboshop &>>$log_file
+
+echo -e "${color} Create Application Directory ${nocolor}"
+rm -rf ${app_path} &>>$log_file
+mkdir ${app_path} &>>$log_file
+
+echo -e "${color} Download Application Content ${nocolor}"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
+cd ${app_path}
+
+echo -e "${color} EXtract Application Content ${nocolor}"
+unzip /tmp/catalogue.zip  &>>$log_file
+cd ${app_path}
+
+echo -e "${color} Install NodeJS Dependencies ${nocolor}" 
+npm install  &>>$log_file
+
+echo -e "${color} Setup SystemD Service ${nocolor}" 
+cp /root/learn-shell/roboshop-shell/${componenet}.service /etc/systemd/system/$component.service &>>$log_file
+
+echo -e "${color} Start $component Service ${nocolor}" 
+systemctl daemon-reload  &>>$log_file
+systemctl enable $component  &>>$log_file
+systemctl restart $component  &>>$log_file
 
 echo -e "${color} Copy MongoDB Repo file ${nocolor}"
 cp /root/learn-shell/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>$log_file
