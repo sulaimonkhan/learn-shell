f9a67ba76b4f123eb2cd339e74c106920e39abf4
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-07 18:23:13 +0530

    aaassss

---
+++ roboshop-shell/01cart.sh
@@ -0,0 +1,22 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+
+rm -rf /app
+mkdir /app 
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+cd /app
+
+unzip /tmp/cart.zip
+cd /app 
+
+npm install 
+
+cp /root/learn-shell/roboshop-shell/cart.service /etc/systemd/system/cart.service
+
+systemctl daemon-reload
+systemctl enable cart 
+systemctl start cart
