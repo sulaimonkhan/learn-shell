9b2711c0cac22a32233276f2bd472d04cff7ceca
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-09 20:00:53 +0530

    basic

--- roboshop-shell/03-vars.sh
+++ roboshop-shell/03-vars.sh
@@ -4,3 +4,10 @@ echo value of a is $a dollars
 
 # Alternatively variable also can be accessed by using ${a}
 echo value of a is ${a}dollares
+
+# special variable for inputs
+# special variable used for inputs are $0 to $n, $* , $#
+echo $0
+echo $1
+echo $*
+echo $#
