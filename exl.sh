b743c07750ce48de1116ce920111bcf5acb644b8
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-11 17:45:41 +0530

    aaaww

---
+++ exl.sh
@@ -0,0 +1,4 @@
+for componented in fronted mongodb catalogue redis user cart mysql shipping rabbitmq payment dispatch ; do
+  echo creating server - $component
+  aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type t3.small
+done
