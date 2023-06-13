e67bff9ffb4d4243f1bbc305694084dce9223577
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-13 12:51:34 +0530

    aaa

--- roboshop-shell/payment.sh
+++ roboshop-shell/payment.sh
@@ -2,7 +2,7 @@ source common.sh
 component=payment
 
 roboshop_app_password=$1
-if [ -z "$roboshop_app_password"];then
+if [ -z "$roboshop_app_password"]; then
   echo roboshop_app_password is missing
   exit 1
 fi  
