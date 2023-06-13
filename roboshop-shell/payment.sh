848cfeee3d333a16fb0c09963243d9bc3aefc9de
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-13 12:55:34 +0530

    aaa

--- roboshop-shell/payment.sh
+++ roboshop-shell/payment.sh
@@ -3,7 +3,7 @@ component=payment
 
 roboshop_app_password=$1
 if [ -z "$roboshop_app_password" ]; then
-  echo roboshop_app_password is missing
+  echo "roboshop_app_password is missing"
   exit 1
 fi  
 python
