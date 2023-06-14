84c9f9a329256d0561c528567b3f076eab74dfbf
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-14 17:59:06 +0530

    aaaa

--- roboshop-shell/mediawikki/appachi.sh
+++ roboshop-shell/mediawikki/appachi.sh
@@ -1,5 +1,6 @@
 cd /tmp
-curl -O https://releases.wikimedia.org/mediawiki/1.39/mediawiki-1.39.3.tar.gz
+url="https://releases.wikimedia.org/mediawiki/1.39/mediawiki-1.39.3.tar.gz
+curl -O $url
 yum install httpd -y
 cd /var/www/html
 tar -xf /tmp/mediawiki-1.39.3.tar.gz
