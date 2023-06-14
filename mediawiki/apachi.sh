e06ee25347f113bcaeb693767fc245a2198d0a7b
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-06-14 18:22:38 +0530

    aaa

--- mediawiki/apachi.sh
+++ mediawiki/apachi.sh
@@ -1,5 +1,5 @@
 cd /tmp
-url="https://releases.wikimedia.org/mediawiki/1.39/mediawiki-1.39.3.tar.gz
+url="https://releases.wikimedia.org/mediawiki/1.39/mediawiki-1.39.3.tar.gz"
 curl -O $url
 yum install httpd -y
 cd /var/www/html
