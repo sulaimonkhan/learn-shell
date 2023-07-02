ebae802ed684c0ef522d282696c8854d00b9ead2
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-07-02 14:20:59 +0530

    qqqq

--- learn-terraform/04-data-sources/data.tf
+++ learn-terraform/04-data-sources/data.tf
@@ -9,5 +9,6 @@ data "aws_ec2_spot_price" "example" {
 }
 
 output "price" {
-  value = data.aws_ec2_instance_spot_price.example.spot_price    
+  value = data.aws_ec2_spot_price.example.spot_price     
 }
+
