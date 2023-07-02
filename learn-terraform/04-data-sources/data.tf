579e333c600afd16e755e7d571fb54ef93d30911
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-07-02 14:17:50 +0530

    aaaa

--- learn-terraform/04-data-sources/data.tf
+++ learn-terraform/04-data-sources/data.tf
@@ -1,11 +1,11 @@
-data "aws_ec2_instance_spot_price" "example" {
+data "aws_ec2_spot_price" "example" {
   instance_type     = "t3.medium"
   availability_zone = "us-east-1a"
 
   filter {
     name   = "product-description"
     values = ["Linux/UNIX"]
-  }
+  }  
 }
 
 output "price" {
