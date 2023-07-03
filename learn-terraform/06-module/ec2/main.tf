98bb03bb857f029068f882e82490397a8419aa0a
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-07-03 12:40:55 +0530

    sss

--- learn-terraform/06-module/ec2/main.tf
+++ learn-terraform/06-module/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group_sg.id]
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     Name = "sample"
   }
