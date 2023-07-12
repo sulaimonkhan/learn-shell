e0de1ef7e6576c759166d81d99971c3d77e0dcf4
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-07-12 13:27:54 +0530

    demo

--- learn-terraform/08-provisoner/main.tf
+++ learn-terraform/08-provisoner/main.tf
@@ -8,25 +8,29 @@ resource "aws_instance" "web" {
   }
 }
 
-provisioner "remote-exec" {
+  provisioner "remote-exec" {
   
    connection {
-    type     = "ssh"
-    user     = "centos"
-    password = DevOps321
-    host     = self.public_ip
+     type     = "ssh"
+     user     = "centos"
+     password = "DevOps321"
+     host     = self.public_ip
   }
+  
   
   inline = [
       "sudo labauto ansible"
-      " ansible-pull -i localhost, -U http://github.com/https://github.com/sulaimonkhan/roboshop-ansible mai.yml -e env-dev -e role_name=frontend 
+      "ansible-pull -i localhost, -U https://https://github.com/sulaimonkhan/roboshop-ansible main.yml -e env-dev -e role_name=frontend 
     ]
   }
 
 
+}
+
+
  
 data "aws_ami" "example" {
-  owners = ["973714476881"] 
+  owners           = ["973714476881"] 
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice" 
 }
