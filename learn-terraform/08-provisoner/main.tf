a72f17c95653ecfa2a9089ed46b04c14eb0e8ba6
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-07-12 13:05:24 +0530

    demo

---
+++ learn-terraform/08-provisoner/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-019bff0781dca161f"]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+provisioner "remote-exec" {
+  inline = [
+      "sudo labauto ansible"
+      " ansible-pull -i localhost, -U http://github.com/https://github.com/sulaimonkhan/roboshop-ansible mai.yml -e env-dev -e role_name=frontend 
+    ]
+  }
+
+
+ 
+data "aws_ami" "example" {
+  owners = ["973714476881"] 
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice" 
+}
