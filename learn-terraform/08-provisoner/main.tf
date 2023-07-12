ad79a181a55bca88fa6c14c1dc9e34911dc02b6a
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-07-12 16:23:56 +0530

    asasas

--- learn-terraform/08-provisoner/main.tf
+++ learn-terraform/08-provisoner/main.tf
@@ -24,9 +24,8 @@ resource "aws_instance" "web" {
    
 }
 
-
 data "aws_ami" "example" {
-  owners      = ["973714476881"] 
+  owners = ["973714476881"]
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"   
-}
+  name_regex = "Centos-8-DevOps-Practice"  
+} 
