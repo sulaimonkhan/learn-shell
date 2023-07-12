ea0a0811e4f7442aae703efd6be4556006e6c7a2
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-07-12 15:36:21 +0530

    aaa

--- learn-terraform/08-provisoner/main.tf
+++ learn-terraform/08-provisoner/main.tf
@@ -3,31 +3,30 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-019bff0781dca161f"]
 
+
   tags = {
     Name = "HelloWorld"
   }
-}
-
-  provisioner "remote-exec" {
-  
-   connection {
-     type     = "ssh"
-     user     = "centos"
-     password = "DevOps321"
-     host     = self.public_ip
-    }
-  
+   provisioner "remote-exec" {
+    
+     connection {
+       type     = "ssh"
+       user     = "centos"
+       password = DevOps321
+       host     = self.public_ip
+     }
 
     inline = [
-      "sudo labauto ansible"
-      "ansible-pull -i localhost, -U https://https://github.com/sulaimonkhan/roboshop-ansible main.yml -e env-dev -e role_name=frontend 
+      "sudo labauto ansible",
+      "ansible-pull" -i localhost, -U https://https://github.com/sulaimonkhan/robpshop-ansible main.yml -e env-dev -e role_name=frontend 
     ]
   }
-
+   
 }
- 
+
+
 data "aws_ami" "example" {
   owners           = ["973714476881"] 
   most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice" 
+  name_regex       = "Centos-8-DevOps-Practice"   
 }
