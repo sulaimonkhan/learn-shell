0d98b6726166827f6c2dbca6240e78117e7cc5d7
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-10-22 20:21:54 +0530

    color

--- 02-print.sh
+++ 02-print.sh
@@ -2,3 +2,17 @@
 
 echo Hello world
 echo sohail
+
+# While printing some times to grab the attention of user we might need to print in some color 
+# syntax : echo -e "\e[COLmMESSAGE\e[0m"
+# -e - enable colors
+# \e[COLm - To enable certain color 
+#\e[0m - To disable color which is enable 
+# COL stands for color and possible colors are RED(31), GREEN(32), YELLOW(33), BlUE(34), MAGENTA(35), CYAN(36)
+
+echo -e "\e[31msohail in Red color\e[0m"
+echo -e "\e[31msohail in Green color\e[0m"
+echo -e "\e[31msohail in Yellow color\e[0m"
+echo -e "\e[31msohail in Blue color\e[0m"
+echo -e "\e[31msohail in Magenta color\e[0m"
+echo -e "\e[31msohail in Cyan color\e[0m"
